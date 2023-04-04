31bd28ec0fb7cf2515877351d6d56f2bda029526
Bobrov Ilia <bobrov.ilia@list.ru>
2023-04-04 15:26:40 +0500

    task 2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -4,8 +4,13 @@ function calcShipping(sum, min, shipping) {
     let shippingPrice = shipping; // стоимость доставки
 
     // Задание №2.1. Рассчитать доставку
-
-    // создайте переменную shippingSum
+    let shippingSum;
+    if (productsSum == 0 || productsSum >= freeShippingMinSum){
+        shippingSum = 0;
+    }else {
+        shippingSum = shippingPrice;
+    }        
+        // создайте переменную shippingSum
 
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
@@ -27,6 +32,12 @@ function calcDiscount(sum, min, discount) {
     let discountPart = discount; // величина скидки в процентах
 
     // Задание №2.2. Рассчитать скидку
+    let discountSum;
+    if (productsSum >= discountMinSum){
+        discountSum = productsSum / 100 * discountPart;
+    }else{
+        discountSum = 0;
+    }
 
     // создайте переменную discountSum
 
@@ -44,13 +55,17 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     let discountSum = calcDiscount(sum, discountMinSum, discountPart);
 
     // Задача №2.3. Рассчитать скидки и доставку в корзине
-
+    let totalSum = productsSum;
+    totalSum = totalSum - discountSum;
     // создайте переменную totalSum
 
     // присвойте totalSum значение productsSum
     // уменьшите totalSum на discountSum
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
+    totalSum = totalSum + shippingSum;
+    let freeShipping;
+    freeShipping =!shippingSum;
 
     // прибавьте к totalSum значение shippingSum
 
